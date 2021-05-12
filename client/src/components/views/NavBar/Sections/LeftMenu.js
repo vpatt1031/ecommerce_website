de5a706428e7e134b4c08c390738c39cb391b79c
Vanessa Patterson <vpatt1031@gmail.com>
2021-05-12 10:42:42 -0400

    blog link test

--- client/src/components/views/NavBar/Sections/LeftMenu.js
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Menu } from 'antd';
-import AnchorLink from 'antd/lib/anchor/AnchorLink';
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
@@ -12,9 +11,7 @@ function LeftMenu(props) {
     </Menu.Item>
     <SubMenu title={<span>Blogs</span>}>
       <MenuItemGroup title="Covid Travel Guidelines">
-          <Menu.Item 
-          containerElement={<AnchorLink to="https://www.cdc.gov/coronavirus/2019-ncov/travelers/index.html"/>}
-          key="setting:1">Travel by Air</Menu.Item>
+          <Menu.Item key="mail"><a href="https://www.cdc.gov/coronavirus/2019-ncov/travelers/faqs.html">Travel During Covid</a></Menu.Item>
         <Menu.Item key="setting:2">Park Guidelines</Menu.Item>
       </MenuItemGroup>
       <MenuItemGroup title="Travel Insurance">
